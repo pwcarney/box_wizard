dbcfd5e8d1eb1c0ea09e799900d79f57dfc51bee
whatPanache <pwcarney@bu.edu>
2017-01-24 18:23:43 -0500

    Spawns boxes!

--- box_wizard/Assets/Scripts/PlayerController.cs
+++ box_wizard/Assets/Scripts/PlayerController.cs
@@ -6,12 +6,15 @@ public class PlayerController : MonoBehaviour {
 
 	public float maxSpeed;
 	public float jumpPower;
+	private bool facingRight = false;
 
 	private Rigidbody2D rb2d;
 
 	public Transform groundCheck;
 	float groundRadius = 0.1f;
 	public LayerMask whatIsGround;
+
+	public GameObject boxPrefab;
 
 	void Start()
 	{
@@ -20,6 +23,17 @@ public class PlayerController : MonoBehaviour {
 
 	void Update()
 	{
+		if (Input.GetButtonDown("Box")) 
+		{
+			float boxSpawnOffset = 0f;
+			if (facingRight)
+				boxSpawnOffset = 1.7f;
+			else if (!facingRight)
+				boxSpawnOffset = -1.7f;
+				
+			GameObject box = Instantiate (boxPrefab, new Vector3 (transform.position.x + boxSpawnOffset, transform.position.y), Quaternion.identity);
+		}
+
 		if (IsGrounded() && Input.GetButtonDown("Jump")) 
 		{
 			rb2d.AddForce (new Vector2 (0f, jumpPower));
@@ -28,11 +42,26 @@ public class PlayerController : MonoBehaviour {
 
 	void FixedUpdate()
 	{
-		rb2d.velocity = new Vector2(Input.GetAxisRaw ("Horizontal") * maxSpeed, rb2d.velocity.y);
+		float move = Input.GetAxisRaw ("Horizontal");
+
+		rb2d.velocity = new Vector2(move * maxSpeed, rb2d.velocity.y);
+
+		if (move > 0 && !facingRight)
+			Flip ();
+		else if (move < 0 && facingRight)
+			Flip ();
 	}
 		
 	bool IsGrounded()
 	{
 		return Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+	}
+
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 myScale = transform.localScale;
+		myScale.x *= -1;
+		transform.localScale = myScale;
 	}
 }
